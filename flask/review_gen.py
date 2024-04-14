8ec225e3207b4395cee7dc46efe4ae630517860e
Elian <elian.dap@outlook.com>
2024-04-14 07:16:00 -0400

    amend requirements

--- flask/review_gen.py
+++ flask/review_gen.py
@@ -1,10 +1,6 @@
 from app import *
 
 def generate_reviews_for_profs():
-    profs = Professor.objects()
-    for prof in profs:
-        if not prof.reviews:
-            print(f"Generating reviews for {prof.name}")
-            prof.generate_reviews()
-        else:
-            print(f"Reviews already exist for {prof.name}")
+    # select prof pid 5 and leave review
+    professor = Professor.objects(pid=5).first()
+    professor.add_review("Great professor!", )
