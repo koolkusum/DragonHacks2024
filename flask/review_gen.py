5959f8b19203674263425590022c865ecd07354a
Elian <elian.dap@outlook.com>
2024-04-14 09:49:55 -0400

    fix nav for resume

--- flask/review_gen.py
+++ flask/review_gen.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 def generate_reviews_for_profs():
     # for each course of professor get name and description
     prompt = """Generate a review for a professor that relates to the following topics below. Emulate the language a student would use on the casual, this is a place for students to conversate with one another and stay in first person.
-    Provide a title and description, follow the following json format do not edit or you will be terminated. {"title:" "", "description:" ""}
+    Provide a title and description, follow the following json format do not edit or you will be terminated. {"title:" "", "description:" ""}g
     Separate each json entry with a ',' and do not include any spaces between the json entries.
     Do at most three reviews per professor, include some randomness in positivity and negativity, but keep it civil.
      The professor is """
